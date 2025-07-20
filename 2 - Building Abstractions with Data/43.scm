4c5c2ef062848e7e7578933ec380c64d5fea913b
Wallysson Oliveira <wallyssonryu@gmail.com>
2025-07-19 22:05:48 -0300

    FIX: answer

--- 2 - Building Abstractions with Data/43.scm
+++ 2 - Building Abstractions with Data/43.scm
@@ -2,8 +2,8 @@
 
 #|
    The problem with this exchange is that the queen-col recursions will occur for every
-  will occur for every element of (enumerate-interval 1 board-size), so it will occur for a
-  for a number of board-size elements.
+  element of (enumerate-interval 1 board-size), so it will occur for a number of
+  board-size elements.
 
    Thus, the first version runs in O(n!), while the Louis version runs in
   in O(n^n), where n = board size. So, in summary, the Louis version is exponentially
